d31382a0af5e3e2b3e1b092e3fab191a0e0edfa3
Diamantino-Op <diamantino.op@gmail.com>
2025-09-30 23:43:26 +0200

    Update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -38,19 +38,9 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
-    keyMap = "it";
-    # useXkbConfig = true; # use xkb.options in tty.
+    # keyMap = "it";
+    useXkbConfig = true; # use xkb.options in tty.
   };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "it";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -51,6 +51,10 @@ in {
   services.displayManager.sddm.enable = true;
   services.xserver.enable = true;
 
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "it";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
+
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   services.pipewire = {
